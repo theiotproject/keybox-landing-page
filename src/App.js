bb12dc628f1e6b8da734ace1436e4ee372152784
Ania <nnakocaj@gmail.com>
2023-07-31 16:10:50 +0200

    Changes in the theme and responsive design.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Keybox2 from "./assets/keybox2.png";
 import Keybox1 from "./assets/keybox1.png";
 import Logo from "./assets/keybox.png";
 import { Squeeze as Hamburger } from "hamburger-react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   const [open, setOpen] = React.useState(false);
@@ -19,52 +18,73 @@ function App() {
 
   const goOne = () => {
     one.current?.scrollIntoView({ behavior: "smooth" });
-    setOpen(!open);
   };
 
   const goTwo = () => {
     two.current?.scrollIntoView({ behavior: "smooth" });
-    setOpen(!open);
   };
 
   const goThree = () => {
     three.current?.scrollIntoView({ behavior: "smooth" });
-    setOpen(!open);
   };
 
   const goFour = () => {
     four.current?.scrollIntoView({ behavior: "smooth" });
-    setOpen(!open);
   };
 
   return (
     <div className="home-mobile">
-      <header className="header">
-        <img className="logo" alt="Keybox" src={Logo} />
+      <div className="header-mobile">
+        <header className="header">
+          <img className="logo" alt="Keybox" src={Logo} />
 
-        <Hamburger toggled={open} toggle={setOpen} color="rgb(58, 155, 141)" />
-      </header>
-      {open && (
-        <div>
-          <table>
-            <tr>
-              <td onClick={goOne}>Strona Główna</td>
-            </tr>
-            <tr>
-              <td onClick={goTwo}>Dlaczego KeyBox System?</td>
-            </tr>
-            <tr>
-              <td onClick={goThree}>Opis Produktu</td>
-            </tr>
-            <tr>
-              <td onClick={goFour}>Kontakt</td>
-            </tr>
-          </table>
-        </div>
-      )}
+          <Hamburger
+            toggled={open}
+            toggle={setOpen}
+            color="rgb(58, 155, 141)"
+          />
+        </header>
+        {open && (
+          <div>
+            <table>
+              <tr>
+                <td onClick={goOne}>Strona Główna</td>
+              </tr>
+              <tr>
+                <td onClick={goTwo}>Dlaczego KeyBox System?</td>
+              </tr>
+              <tr>
+                <td onClick={goThree}>Opis Produktu</td>
+              </tr>
+              <tr>
+                <td onClick={goFour}>Kontakt</td>
+              </tr>
+            </table>
+          </div>
+        )}
+      </div>
+      <div className="header-desktop">
+        <header className="header">
+          <img className="logo" alt="Keybox" src={Logo} />
+          <div className="navbar">
+            <button onClick={goTwo} className="nav-button">
+              Dlaczego KeyBox System?
+            </button>
+            <button onClick={goThree} className="nav-button">
+              Opis Produktu
+            </button>
+            <button onClick={goFour} className="nav-button">
+              Kontakt
+            </button>
+          </div>
+        </header>
+      </div>
       <div className="one" ref={one}>
-        <img className="render" alt="Keybox" src={Keybox2} />
-        <img className="render" alt="Keybox" src={Keybox1} />
+        <div className="renders">
+          <img className="render" alt="Keybox" src={Keybox2} />
+          <img className="render" id="three" alt="Keybox" src={Keybox3} />
+          <img className="render" alt="Keybox" src={Keybox1} />
+        </div>
         <div className="main-text">
           <h1>KeyBox System</h1>
           <p>zarządzanie kluczami jeszcze nigdy nie było tak łatwe!</p>
@@ -74,8 +94,8 @@ function App() {
         </div>
       </div>
       <div className="two" ref={two}>
+        <div className="h2">dlaczego KeyBox System?</div>
         <div className="teksty">
-          <div className="h2">dlaczego KeyBox System?</div>
           <div className="frame-2">
             <p className="text-wrapper-2">
               ważne klucze zawsze w jednym miejscu
@@ -103,42 +123,18 @@ function App() {
         </div>
       </div>
       <div className="three" ref={three}>
-        <div className="KeyboxSystem" style={{ width: 315, height: 567 }}>
-          <span className="h2">
+        <div className="KeyboxSystem">
+          <span className="h3">
             KeyBox System
             <br />
           </span>
           <span className="text2">
-            {" "}
-            <br />
             to innowacyjne rozwiązanie stworzone specjalnie po to, by ułatwić Ci
-            życie. <br />
-            <br />
-            Już nie musisz martwić się, gdzie zostały schowane ważne klucze lub
-            kto je zabrał. Nasza inteligentna skrzynka zapewnia doskonałe
-            zarządzanie kluczami!
-            <br />
-          </span>
-          <span className="text2">
-            <br />
-            <br />
-          </span>
-          <span className="h2">
-            Jak wygląda to w praktyce? <br />
-          </span>
-          <span className="text2">
-            <br />
-          </span>
-          <span className="text2">
-            Key Box jest zamontowany <br />w jednym miejscu i rejestruje, <br />
-            kto i kiedy pobiera klucz. <br />
-            <br />
-            Masz dostęp do tych informacji <br />
-            dzięki naszym aplikacjom <br />
-            internetowym i mobilnym, <br />a to wszystko w czasie rzeczywistym!
+            życie. Już nie musisz martwić się, gdzie zostały schowane ważne
+            klucze lub kto je zabrał. Nasza inteligentna skrzynka zapewnia
+            doskonałe zarządzanie kluczami!
           </span>
         </div>
-        <img className="keybox-3" alt="Keybox" src={Keybox3} />
       </div>
       <div className="four" ref={four}>
         <form className="formularz-kontaktowy">
@@ -161,17 +157,8 @@ function App() {
       </div>
 
       <div className="footer">
-        <p>dołącz do naszej społeczności!</p>
-        <div className="icons">
-          <FontAwesomeIcon
-            icon="faTwitter"
-            bounce
-            style={{ color: "#424242" }}
-          />
-        </div>
-        <p>
-          Copyright © 2023 KeyBox System <br />| Powered by TheIoTProject
-        </p>
+        <div className="icons"></div>
+        Copyright © 2023 KeyBox System <br />| Powered by TheIoTProject
       </div>
     </div>
   );
